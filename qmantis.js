8b5619f09ccd2cd674c16376eb7f80bbc5d41335
kkrueger19@gmail.com <kkrueger19@gmail.com>
2022-03-18 09:42:15 -0600

    merge conflict fixes

---
+++ qmantis.js
@@ -0,0 +1,27 @@
+
+import "./observability/tracing.js";
+import { countRequests, countErrors, requestLatency } from "./observability/monitoring.js";
+
+const extensions = ({ context, result }) => {
+  let runTime = Date.now() - context.startTime;
+  requestLatency.record(runTime);
+
+  if (result.errors) {
+    console.log("error", result.errors);
+    countErrors();
+  }
+};
+
+const qMantis = (schema, rootValue) => {
+  return (request) => {
+    return {
+      schema,
+      //  rootValue,
+      context: { startTime: Date.now() },
+      graphiql: true,
+      extensions,
+    };
+  };
+};
+
+export { qMantis, countRequests };
