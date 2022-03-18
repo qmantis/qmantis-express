5b228177911e3cee8fbf9834caf0e815194c5e5c
Emily Ma <em.kayama@gmail.com>
2022-03-17 20:15:27 -0400

    change package, format files
    Co-authored-by: Lisa Melo <lisa-m1@users.noreply.github.com>
    Co-authored-by: kalkrueger <kalkrueger@users.noreply.github.com>
    Co-authored-by: Kathy Fregoso <kathyfregoso@users.noreply.github.com>

--- observability/monitoring.js
+++ observability/monitoring.js
@@ -1,34 +1,34 @@
-import { MeterProvider } from '@opentelemetry/sdk-metrics-base';
-import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
+import { MeterProvider } from "@opentelemetry/sdk-metrics-base";
+import { PrometheusExporter } from "@opentelemetry/exporter-prometheus";
 
-const options = {port: 9464, startServer: true};
+const options = { port: 9464, startServer: true };
 const exporter = new PrometheusExporter(options);
 
 const meter = new MeterProvider({
   exporter,
   interval: 1000,
-}).getMeter('example-prometheus');
+}).getMeter("example-prometheus");
 
-const requestsCounter = meter.createCounter('total_requests', {
-  description: 'Counting all requests made to /graphql'
+const requestsCounter = meter.createCounter("total_requests", {
+  description: "Counting all requests made to /graphql",
 });
 
-const errorsCounter = meter.createCounter('total_errors', {
-  description: 'Counting all errors'
+const errorsCounter = meter.createCounter("total_errors", {
+  description: "Counting all errors",
 });
 
 const requestLatency = meter.createHistogram("request_latency", {
   description: "Record latency for incoming requests",
-  boundaries: [10, 50, 100, 150, 200, 250, 300, 400, 500], 
+  boundaries: [10, 50, 100, 150, 200, 250, 300, 400, 500],
 });
 
 const countRequests = (req, res, next) => {
   requestsCounter.add(1);
   next();
-}
+};
 
 const countErrors = () => {
   errorsCounter.add(1);
-}
+};
 
-export { countRequests, countErrors, requestLatency }
+export { countRequests, countErrors, requestLatency };
