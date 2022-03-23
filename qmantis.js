01cf37f7e8ccda856c0137c4f02884450e4eb644
Lisa Melo <melo.nutrition@gmail.com>
2022-03-23 09:38:14 -0400

    cleaned up files
    Co-authored-by: Kathy Fregoso <kathyfregoso@users.noreply.github.com>
    Co-authored-by: Emily Siu <eemkey@users.noreply.github.com>
    Co-authored-by: kalkrueger <kalkrueger@users.noreply.github.com>

--- qmantis.js
+++ qmantis.js
@@ -1,33 +1,7 @@
 import "./observability/tracing.js";
-import { countRequests, requestLatency } from "./observability/monitoring.js";
-import config from "./utils.js";
 import { qMantisServer } from "./server/server.js"
 
 
-const extensions = ({ context, result, operationName }) => {
-  let runTime = Date.now() - context.startTime;
-  requestLatency.record(runTime);
 
-  config.operationName = operationName || "operation";
 
-  if (result.errors) {
-    console.log("error", result.errors);
-    config.error = true;
-  } else {
-    config.error = false;
-  }
-};
-
-const qMantis = (schema, rootValue) => {
-  return () => {
-    return {
-      schema,
-      //  rootValue,
-      context: { startTime: Date.now() },
-      graphiql: true,
-      extensions,
-    };
-  };
-};
-
-export { qMantisServer, countRequests };
+export { qMantisServer };
