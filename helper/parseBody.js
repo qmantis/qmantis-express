58491caa67fbbaa92da882e2fcd3bda820f316c4
Lisa Melo <melo.nutrition@gmail.com>
2022-03-23 14:22:01 -0400

    removed comments from parseBody file
    Co-authored-by: Kathy Fregoso <kathyfregoso@users.noreply.github.com>
    Co-authored-by: Emily Siu <eemkey@users.noreply.github.com>
    Co-authored-by: kalkrueger <kalkrueger@users.noreply.github.com>

--- helper/parseBody.js
+++ helper/parseBody.js
@@ -6,39 +6,29 @@ import httpError from 'http-errors';
 import contentType from 'content-type';
 
 
-
-/**
- * Provided a "Request" provided by express or connect (typically a node style
- * HTTPClientRequest), Promise the body data contained.
- */
 export async function parseBody(req) {
   const { body } = req;
 
-  // If express has already parsed a body as a keyed object, use it.
   if (typeof body === 'object' && !(body instanceof Buffer)) {
     return body;
   }
 
-  // Skip requests without content types.
   if (req.headers['content-type'] === undefined) {
     return {};
   }
 
   const typeInfo = contentType.parse(req);
 
-  // If express has already parsed a body as a string, and the content-type
-  // was application/graphql, parse the string body.
   if (typeof body === 'string' && typeInfo.type === 'application/graphql') {
     return { query: body };
   }
 
-  // Already parsed body we didn't recognise? Parse nothing.
   if (body != null) {
     return {};
   }
 
   const rawBody = await readBody(req, typeInfo);
-  // Use the correct body parser based on Content-Type header.
+
   switch (typeInfo.type) {
     case 'application/graphql':
       return { query: rawBody };
@@ -47,7 +37,6 @@ export async function parseBody(req) {
         try {
           return JSON.parse(rawBody);
         } catch {
-          // Do nothing
         }
       }
       throw httpError(400, 'POST body sent invalid JSON.');
@@ -55,31 +44,18 @@ export async function parseBody(req) {
       return querystring.parse(rawBody);
   }
 
-  // If no Content-Type header matches, parse nothing.
   return {};
 }
 
-/**
- * RegExp to match an Object-opening brace "{" as the first non-space
- * in a string. Allowed whitespace is defined in RFC 7159:
- *
- *     ' '   Space
- *     '\t'  Horizontal tab
- *     '\n'  Line feed or New line
- *     '\r'  Carriage return
- */
 const jsonObjRegex = /^[ \t\n\r]*\{/;
 
-// Read and parse a request body.
 async function readBody(req, typeInfo) {
   const charset = typeInfo.parameters.charset?.toLowerCase() ?? 'utf-8';
 
-  // Assert charset encoding per JSON RFC 7159 sec 8.1
   if (charset !== 'utf8' && charset !== 'utf-8' && charset !== 'utf16le') {
     throw httpError(415, `Unsupported charset "${charset.toUpperCase()}".`);
   }
 
-  // Get content-encoding (e.g. gzip)
   const contentEncoding = req.headers['content-encoding'];
   const encoding =
     typeof contentEncoding === 'string'
@@ -88,12 +64,10 @@ async function readBody(req, typeInfo) {
   const maxBuffer = 100 * 1024; // 100kb
   const stream = decompressed(req, encoding);
 
-  // Read body from stream.
   try {
     const buffer = await getStream.buffer(stream, { maxBuffer });
     return buffer.toString(charset);
   } catch (rawError) {
-    /* istanbul ignore else: Thrown by underlying library. */
     if (rawError instanceof MaxBufferError) {
       throw httpError(413, 'Invalid body: request entity too large.');
     } else {
@@ -104,7 +78,6 @@ async function readBody(req, typeInfo) {
   }
 }
 
-// Return a decompressed stream, given an encoding.
 function decompressed(req, encoding) {
   switch (encoding) {
     case 'identity':
